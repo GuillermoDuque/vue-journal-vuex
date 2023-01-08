8583b71b0b753634f2bb7f6da5132bd385ac1c86
g.duque <guillermo.duque@ginkofs.com>
2023-01-08 19:33:00 -0300

    se sube codigo hasta clase 170

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: '1',
+      date: new Date().toDateString(),
+      text: "Voluptate laborum in deserunt sit consequat laboris sit dolore aute dolor labore labore commodo. Exercitation nulla enim cillum est est nulla eiusmod adipisicing anim exercitation Lorem laboris. Consequat commodo tempor id tempor esse dolor eu exercitation mollit qui velit adipisicing id fugiat.",
+      picture: null,
+    },
+    {
+        id: '2',
+        date: new Date().toDateString(),
+        text: "Dolore sunt minim min im et eu aliquip eiusmod qui.",
+        picture: null,
+        
+      },{
+        id: '3',
+        date: new Date().toDateString(),
+        text: "Ea laboris anim ex amet consequat ex nisi.",
+        picture: null,
+      },
+    
+  ],
+});
