57effbd15bafb43e13e33befccec3d04b398b171
g.duque <guillermo.duque@ginkofs.com>
2023-01-20 06:02:35 -0300

    Se añade axios y se cargan entradas desde  firebase

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -15,12 +15,19 @@
 <script>
 
 import {  defineAsyncComponent } from "vue";
+import { mapActions } from "vuex";
 
 export default{
     components:{
             NavBar: defineAsyncComponent(() => import('../components/NavBar')),
             EntryList: defineAsyncComponent(() => import('../components/EntryList'))
 
+    },
+    methods:{
+        ...mapActions('journal',['loadEntries'])
+    },
+    created(){
+        this.loadEntries()
     }
 
 }
--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -1,9 +1,20 @@
 // export const myAction = async ({commit}) =>{
-    
+
+import journalApi from "@/api/journalApi"
+
 // }
 
-export const loadEntries = async (/*{commit}*/) =>{
+export const loadEntries = async ({commit}) =>{
     
+    const { data } = await journalApi.get('/entries.json')
+    const entries = []
+    for(let id of Object.keys( data )){
+        entries.push({
+            id,
+            ...data[id]
+        })
+    }
+    commit('setEntries', entries)
 }
 
 export const updateEntries = async (/*{commit}*/) =>{
