b89c55db08af200a17b8abd84d3e2c6e0befa989
g.duque <guillermo.duque@ginkofs.com>
2023-02-01 06:25:04 -0300

    ADD: test uploadImage carga de archivo

---
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import uploadImage from "@/modules/daybook/helpers/uploadImage";
+
+describe('Pruebas en el uploadImage',() =>{
+    
+    test('debe de cargar un archivo y retornar el url', async ()=>{
+      const {data} =  await axios.get('https://res.cloudinary.com/dkolaarve/image/upload/v1674639311/xpfejnqk3nemyb1cq7dh.png',{
+        responseType:'arraybuffer'
+    })
+        const file = new File([data],'foto.png')
+
+        const url = await uploadImage(file)
+        expect(typeof url).toBe('string')
+    })
+})
