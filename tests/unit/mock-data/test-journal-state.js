2ab0012874651965b079f049a7c0691c617566b2
g.duque <guillermo.duque@ginkofs.com>
2023-02-05 20:11:46 -0300

    ADD: Test Store

---
+++ tests/unit/mock-data/test-journal-state.js
@@ -0,0 +1,17 @@
+export const journalState = {
+  isLoading: false,
+  entries: [
+    {
+      id: "-NMY-pIE2cTCj8EvyPlU",
+      date: 1674550917901,
+      text: "Hola mundo desde mock data",
+    },
+    {
+      id: "-NMcH-K6kW8buD0t5sTH",
+      date: 1674639297031,
+      picture:
+        "https://res.cloudinary.com/dkolaarve/image/upload/v1674639311/xpfejnqk3nemyb1cq7dh.png",
+      text: "Esta es la segunda entrada",
+    },
+  ],
+};
--- tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -3,13 +3,18 @@ import uploadImage from "@/modules/daybook/helpers/uploadImage";
 
 describe('Pruebas en el uploadImage',() =>{
     
-    test('debe de cargar un archivo y retornar el url', async ()=>{
+    test('debe de cargar un archivo y retornar el url', async ( )=>{
       const {data} =  await axios.get('https://res.cloudinary.com/dkolaarve/image/upload/v1674639311/xpfejnqk3nemyb1cq7dh.png',{
         responseType:'arraybuffer'
     })
-        const file = new File([data],'foto.png')
+      /*  const file = new File([data],'foto.png')
 
         const url = await uploadImage(file)
         expect(typeof url).toBe('string')
+
+        const segments = url.split('/')
+        const imageId = segments[segments.length-1].replace('.jpg','')
+        cloudinary.v2.api.delete_resources(imageId)*/
+         
     })
 })
